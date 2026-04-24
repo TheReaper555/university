b94eabe0eb7b8d48d58e3d0dbd628a74028cfd27
Your Name <you@example.com>
2026-04-23 19:11:03 -0500

    Se agrego gitignore

---
+++ frontend/index.js
@@ -0,0 +1,32 @@
+const API_URL = "http://localhost:8000/students";
+
+document.addEventListener('DOMContentLoaded', () =>{
+
+})
+
+function saveStudents(){
+ const id = document.getElementById("student-id").value;
+ const name = document.getElementById("name").value;
+ const age = parseInt(document.getElementById("age").value);
+ const grade = parseFloat(document.getElementById("grade").value);
+
+ const studentData = {name, age, grade};
+
+ const method = id ? "PUT" : "POST";
+ const url = id ? `${API_URL}/${id}`: API_URL;
+ fetch(url, {
+    method: method,
+    headers: {
+        'Content-Type': 'application/json' //MIME
+    },
+    body: JSON.stringify(studentData)
+ }).then(response => {
+    if (!response.ok){
+        return response.json().then(err =>{throw new Error(err.detail || 'Error en la operacion')})
+    }
+    return response.json()
+ }).then(data =>{
+    alert("Estudiante creado")
+ })
+
+}
