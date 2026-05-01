f9f051f7db2cdecf7f3f389df8865f1660cb15c8
Santiago Uribe <santiagojoseu4@gmail.com>
2026-04-30 23:08:09 -0500

    Cambios en el database

--- database.py
+++ database.py
@@ -1,10 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-SQLALCHEMY_DB_URL = "sqlite:///./students.db"
+# En Render usa /tmp que persiste mientras el servidor está activo
+# En local usa el archivo normal
+if os.environ.get("RENDER"):
+    SQLALCHEMY_DB_URL = "sqlite:////tmp/students.db"
+else:
+    SQLALCHEMY_DB_URL = "sqlite:///./students.db"
 
 engine = create_engine(
-    SQLALCHEMY_DB_URL, connect_args={"check_same_thread":False}
+    SQLALCHEMY_DB_URL, connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
